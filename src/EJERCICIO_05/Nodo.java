a28f6df8186aad2e4b4bbc2933b555601b770477
EduardoJesusGagoRamos <wilberdymr2004@gmail.com>
2022-11-08 23:55:57 -0500

    EJERCICIOS DE LISTAS RESUELTOS

---
+++ src/EJERCICIO_05/Nodo.java
@@ -0,0 +1,46 @@
+//NODO
+package EJERCICIO_05;
+
+/**
+ *
+ * @author GAGO RAMOS EDUARDO JESÚS
+ */
+public class Nodo {
+
+    private double info;
+    private Nodo siguiente;
+
+    public Nodo() {
+        siguiente = null;
+    }
+
+    public Nodo(double x) {
+        siguiente = null;
+        info = x;
+    }
+
+    public Nodo(double x, Nodo siguiente) {
+        info = x;
+        this.siguiente = siguiente;
+    }
+
+    public double getInfo() {
+        return info;
+    }
+
+    public void setInfo(double info) {
+        this.info = info;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public void VerNodo() {
+        System.out.println("[" + info + "]");
+    }
+}
